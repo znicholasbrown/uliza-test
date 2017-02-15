015f51c9a2f09cb52f94d926fa3649d7e68d76bc
System <none>
2017-02-15 14:25:17 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -1,9 +1,6 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
 $(function() {
   $.get('/users', function(users) {
     users.forEach(function(user) {
@@ -13,8 +10,8 @@ $(function() {
 
   $('form').submit(function(event) {
     event.preventDefault();
-    fName = $('input#fName').val();
-    lName = $('input#lName').val();
+    var fName = $('input#fName').val();
+    var lName = $('input#lName').val();
     $.post('/users?' + $.param({fName:fName, lName:lName}), function() {
       $('<li></li>').text(fName + " " + lName).appendTo('ul#users');
       $('input#fName').val('');
